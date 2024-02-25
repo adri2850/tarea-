7e47e31c407cc394684a37e774cc8d0b9ab14994
adri2850 <161059086+adri2850@users.noreply.github.com>
2024-02-25 17:26:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
+import streamlit as st
+import pandas as pd
+#import matplot.pyplot as plt
 
+st.write("Estadisticas de goles de Messi")
+
+# Cargar los datos desde la pagina web
+pagina = "https://players.fcbarcelona.com/es/jugador/548-messi-lionel-andres-messi-cuccitini"
+tablas = pd.read_html(pagina)
+
+# Creac la aplicacion web Streamlit
+st.write(tabla[0].head(5))
 
