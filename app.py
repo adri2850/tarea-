45b47b7ffb7c1c17169a306471fd6557626eb241
adri2850 <161059086+adri2850@users.noreply.github.com>
2024-02-25 16:20:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,2 @@
-from rdkit import Chem
-from rdkit.Chem.Draw import MolsToGridImage
-#molécula random
-molecula = Chem.MolFromSmiles('O=C1CN(C(OC(C)(C)C)=O)[C@](C=C1)([H])CC2=CN(S(=O)(C3=CC=C(C)C=C3)=O)C4=CC=CC=C42')
-molecula
+
+
