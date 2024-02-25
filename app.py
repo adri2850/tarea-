79d2154404c95a4344546c16629528108c4df08a
adri2850 <161059086+adri2850@users.noreply.github.com>
2024-02-25 16:20:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
-Animales-en-Peligro-de-Extincion.jpg
+from rdkit import Chem
+from rdkit.Chem.Draw import MolsToGridImage
+#molécula random
+molecula = Chem.MolFromSmiles('O=C1CN(C(OC(C)(C)C)=O)[C@](C=C1)([H])CC2=CN(S(=O)(C3=CC=C(C)C=C3)=O)C4=CC=CC=C42')
+molecula
